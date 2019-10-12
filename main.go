abce5fb181dbfcee8e8aeb23ec8e27cc1c4e8534
Thaddee Tyl <thaddee.tyl@gmail.com>
2019-10-12 12:03:46 +0200

    Include basic code

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+	"crypto/rand"
+	"time"
+)
+
+var sampleMaxCache = make(map[int]map[int]int64)
+var sampleCache = make(map[int]map[int]int)
+
+type sampling struct {
+	max int64
+	size int
+}
+
+// sampleMax computes the number of samples to get from a single call to rand.Int() to minimize rejection.
+func sampleMax(base int, size int) sampling {
+	if m, ok := sampleMaxCache[base][size]; ok {
+		return sampling{m, sampleCache[base][size]}
+	}
+	//fmt.Printf("base\tsamples\texpected_rejections\n")
+	bestMax := int64(1)
+	bestSampling := 1
+	minRejections := math.Inf(1)
+	var max float64
+	for samples := 1; samples <= size && max < 1 << 50; samples++ {
+		max := math.Pow(float64(base), float64(samples))
+		collision := 1 - max / (math.Pow(2, math.Ceil(math.Log2(max))))
+		expectedRejections := collision * math.Ceil(float64(size/samples))
+		if expectedRejections < minRejections {
+			bestMax = int64(max)
+			bestSampling = samples
+			minRejections = expectedRejections
+		}
+		//fmt.Printf("%d\t%d\t%.3f\n", base, samples, expectedRejections)
+	}
+	if _, ok := sampleMaxCache[base]; !ok {
+		sampleMaxCache[base] = make(map[int]int64)
+		sampleCache[base] = make(map[int]int)
+	}
+	sampleMaxCache[base][size] = bestMax
+	sampleCache[base][size] = bestSampling
+	return sampling{bestMax, bestSampling}
+}
+
+func sample(base int, size int) string {
+	s := ""	
+	sampling := sampleMax(base, size)
+	for i := 0; i < sampling.size; i++ {
+		n, _ := rand.Int(rand.Reader, big.NewInt(sampling.max))
+		s += string(n.String())
+	}
+	return s[:size]
+}
+
+func rawSample(base int, size int) string {
+	s := ""
+	for i := 0; i < size; i++ {
+		n, _ := rand.Int(rand.Reader, big.NewInt(int64(base)))
+		s += string(n.String())
+	}
+	return s
+}
+
+func timed(f func()) time.Duration {
+	start := time.Now()
+	f()
+	return time.Since(start)
+}
+
+func benchmark(n int, f func()) {
+	fmt.Println(timed(func () {
+		for i := 0; i < n; i++ {
+			f()
+		}
+	}))
+}
+
+func main() {
+	tries := 100000
+	base := 10
+	size := 11
+	//sampleMax(base, size)
+	benchmark(tries, func () { rawSample(base, size) })
+	benchmark(tries, func () { sample(base, size) })
+}
